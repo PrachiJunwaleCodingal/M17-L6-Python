33a965682f332a564be654b622cad343c7d4244f
PrachiJunwaleCodingal <prachi.junwale@gmail.com>
2025-01-02 15:28:06 +0530

    Add files via upload

---
+++ act5.py
@@ -0,0 +1,9 @@
+#call fire brigade
+
+ans=input("Enter whether there is fire or not(y/n) ?  ")
+
+if ans == 'Y' or ans == 'y':
+  print("Call fire brigade on 101 ")
+
+else:
+  print("Don't call fire brigade")
